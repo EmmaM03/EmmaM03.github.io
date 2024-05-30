8560c6cb5015afd2e98e3de0157ee682b1898491
EmmaM03 <165604349+EmmaM03@users.noreply.github.com>
2024-05-29 22:01:07 -0300

    Update javascript_registro.js

--- javascript_registro.js
+++ javascript_registro.js
@@ -1,4 +1,3 @@
-// Capturar el evento submit del formulario de registro
 document.getElementById('registroForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Evita que el formulario se envíe de manera tradicional
     
@@ -10,7 +9,7 @@ document.getElementById('registroForm').addEventListener('submit', function(even
     // Crea la cuenta con Firebase Authentication
     firebase.auth().createUserWithEmailAndPassword(emailRegistro, passwordRegistro)
         .then((userCredential) => {
-            // Registro exitoso, puedes redirigir al usuario o realizar otras acciones
+            // Registro exitoso
             console.log("Cuenta creada exitosamente");
             // Redirigir al usuario a la página de inicio de sesión
             window.location.href = "pagina_de_inicio_de_sesion.html"; // Cambia "pagina_de_inicio_de_sesion.html" por la URL de tu página de inicio de sesión
@@ -21,20 +20,6 @@ document.getElementById('registroForm').addEventListener('submit', function(even
         });
 });
 
-// Configuración de tu aplicación Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyAo48jIi_YLbn7fy7fhtTn4wiB7HlIslRY",
-    authDomain: "usuarios-anima-fe499.firebaseapp.com",
-    projectId: "usuarios-anima-fe499",
-    storageBucket: "usuarios-anima-fe499.appspot.com",
-    appId: "1:681582971447:web:87e95f9977e6e29e9c8db2"
-  };
-  
-// Inicializa Firebase con la configuración proporcionada
-const app = firebase.initializeApp(firebaseConfig);
-const storage = firebase.storage();
-  
-// Capturar el evento submit del formulario de inicio de sesión
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Evita que el formulario se envíe de manera tradicional
       
@@ -43,10 +28,9 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const password = document.getElementById('password').value;
       
     // Inicia sesión con Firebase Authentication
-    const auth = firebase.auth();
-    auth.signInWithEmailAndPassword(email, password)
+    firebase.auth().signInWithEmailAndPassword(email, password)
         .then((userCredential) => {
-            // Inicio de sesión exitoso, puedes redirigir al usuario o realizar otras acciones
+            // Inicio de sesión exitoso
             console.log("Inicio de sesión exitoso");
         })
         .catch((error) => {
